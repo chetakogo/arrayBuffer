596a276acfb0971319aa7223b389e13e2b29674f
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2022-05-05 03:44:21 +0300

    version 2.0

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -2,8 +2,7 @@ import ArrayBufferConverter from '../js/arrayBuffer';
 
 test('load data', () => {
   const data = '{ data: { user: { id: 1, name: "Hitman", level: 10 } } }';
-  const arrayBuf = new ArrayBufferConverter();
-  const change = arrayBuf.load(data);
-  const string = arrayBuf.toString(change);
+  const change = ArrayBufferConverter.load(data);
+  const string = ArrayBufferConverter.toString(change);
   expect(string).toEqual('{ data: { user: { id: 1, name: "Hitman", level: 10 } } }');
 });
--- src/js/arrayBuffer.js
+++ src/js/arrayBuffer.js
@@ -1,18 +1,16 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable class-methods-use-this */
 export default class ArrayBufferConverter {
-  load(data) {
+  static load(data) {
     return ((input) => {
       const buffer = new ArrayBuffer(data.length * 2);
       const bufferView = new Uint16Array(buffer);
-      for (let i = 0; i < input.length; i++) {
+      for (let i = 0; i < input.length; i + 1) {
         bufferView[i] = input.charCodeAt(i);
       }
       return buffer;
     })(data);
   }
 
-  toString(data) {
+  static toString(data) {
     const arrayString = new Uint16Array(data);
     return String.fromCharCode(...arrayString);
   }
